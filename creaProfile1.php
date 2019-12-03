ded539c5ee0aba62183048802ea39507cb0bbf23
Yassine Azeba <yassineazebaaz@gmail.com>
2019-12-03 23:14:34 +0100

    creaProfile1.php

--- creaProfile1.php
+++ creaProfile1.php
@@ -8,63 +8,51 @@
 
     <body>
     <section id="sec1">
-        <img id="imgPageDeCo" src="ressources/images/creaProfile1.png">
+        <img id="imgCreaProfil2" src="ressources/images/creaProfile1.png">
     </section>
     <section id="sec2">
         <form method="post" action="t.php" enctype="multipart/form-data">
             <div id="name">
-                <p><label for="Prenom">Prénom :</label>
-                <br>
-                <input type="text" name="Prenom" id="Prenom" placeholder="Votre Prénom"/></p>
-
+                <p><label for="Prenom">Prénom :</label><br>
+                    <input type="text" name="Prenom" id="Prenom" placeholder="Votre Prénom" size="25"/></p>
                 <p><label for="Nom">Nom :</label><br>
-                <input type="text" name="Nom" id="Nom" placeholder="Votre Nom"/></p>
+                <input type="text" name="Nom" id="Nom" placeholder="Votre Nom" size="25"/></p>
             </div>
-                <br />
             <div id="phone">
-                <p><label for="dateNaissance">Date de Naissance :</label><br>
+                <p><label for="dateNaissance">Date de Naissance :</label>
+                <br>
                 <input type="date" name="dateNaissance" id="dateNaissance" placeholder="jj/mm/année"/></p>
-
-                <p><label for="numeroTel">Numéro de téléphone :</label><br>
-                <input type="tel" name="numeroTel" id="numeroTel" placeholder="00.00.00.00.00"/></p>
+                <p><label for="numeroTel">Numéro de téléphone :</label>
+                <br>
+                <input type="tel" name="numeroTel" id="numeroTel" placeholder="00.00.00.00.00" size="25"/>
+                </p>
             </div>
-                <br />
             <div id="mail">
-                <label for="email">Email :</label><br>
-                <input type="email" name="email" id="email"placeholder="Votre adresse mail"/>
+                <p><label for="email">Email :</label><br>
+                <input type="email" name="email" id="email"placeholder="Votre adresse mail" size="50"/></p>
             </div>
-            <br>
             <div id="mdp">
                 <p><label for="passworld"><strong>Mot de passe :</strong></label><br>
-                <input type="password" name="passworld" id="passworld"/></p>
-
-                <p><label for="validePassworld">Confirmation du mot de passe :</label><br>
+                <input type="password" name="passworld" id="passworld"/><br>
+                <label for="validePassworld">Confirmation du mot de passe :</label><br>
                 <input type="password" name="validePassworld" id="validePassworld"/></p>
             </div>
-
-            <p>
-                Type d'emplois : <br/>
-
+            <div id="Trav">
+            <p> Type d'emplois : </p>
                 <input type="radio" name="emplois" value="Pilote" id="Pilote"/>
-                <label  for="Pilote">Pilote</label>
-
+                    <label  for="Pilote">Pilote</label>
                 <input type="radio" name="emplois" value="PersonnelAuSol" id="PersonnelAuSol"/>
-                <label for="PersonnelAuSol">Personnel au sol</label>
-
+                    <label for="PersonnelAuSol">Personnel au sol</label>
                 <input type="radio" name="emplois" value="¨PersonnelNaviguant" id="PersonnelNaviguant"/>
-                <label for="PersonnelNaviguant">Personnel naviguant</label>
-            </p>
-
-            <p>
-                Upload a photo<br/>
-                <input type="file" name="PhotoProfil"/><br/>
-                <input type="submit" value="Envoyer le fichier"/>
-            </p>
-
-            <p>
-                <a href="page_accueil.php">Retour</a>
-                <input type="submit" value="Continuer"/>
-            </p>
+                    <label for="PersonnelNaviguant">Personnel naviguant</label>
+                <br>
+                <p>Upload a photo<br/>
+                <input type="file" name="PhotoProfil" id="PP"/>
+                <input type="submit" value="Envoyer le fichier" id="envoyerPP"/>
+                </p>
+                <p><input type="submit" value="Continuer"/>
+                <a href="page_accueil.php">Retour</a></p>
+            </div>
 
 
         </form>
