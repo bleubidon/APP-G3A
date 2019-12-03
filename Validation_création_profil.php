8486d07a1e78fbffeaf7a9773dd707407ca00026
Charlene <charleneabelard@orange.fr>
2019-12-03 23:02:45 +0100

    Merge branch 'master' of C:\wamp64\www\APP-G3A with conflicts.

---
+++ Validation_création_profil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="Stylesheet" href="ressources/stylesheets/Stylesheet_Login.css" />
+    <title>Validation création profil</title>
+</head>
+
+<section id="sec1">
+    <img id="imgPageDeCo" src="ressources/images/creaProfil3.png">
+</section>
+
+
+
+<?php
+    echo "Prénom : ";
+    echo "Nom : ";
+    echo "Date de naissance : ";
+    echo "Numéro de téléphone : ";
+    
+
+
+
+?>
