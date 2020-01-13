2ab4613ff0d8c92d6375c89c2cf92528276dc130
Yassine Azeba <yassineazebaaz@gmail.com>
2020-01-13 15:55:29 +0100

    faq 1

--- F.A.Q.php
+++ F.A.Q.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="Stylesheet_FAQ.css" />
+    <link rel="stylesheet" href="ressources/stylesheets/Stylesheet.css"/>
     <title>FAQ-Captest</title>
 </head>
+<?php include "views/Mise_en_page.php" ?>
 <body>
 
 <p id="title"><h>Questions & Réponses</h></p>
@@ -12,7 +14,7 @@
 <br>
 
 <ul id="menu-deroulant">
-    <li id="Q"><a href="#">Question 1</a>
+    <li id="Q"><a href="#">↓   Question 1</a>
         <ul>
             <li><a href="#">Réponse 1</a></li>
             <li><a href="#">Réponse 2</a></li>
@@ -20,7 +22,7 @@
         </ul>
     </li>
     <br>
-    <li id="Q"><a href="#">Question 2</a>
+    <li id="Q"><a href="#">↓   Question 2</a>
         <ul>
             <li><a href="#">Réponse 1</a></li>
             <li><a href="#">Réponse 2</a></li>
@@ -28,7 +30,7 @@
         </ul>
     </li>
     <br>
-    <li id="Q"><a href="#">Question 3</a>
+    <li id="Q"><a href="#">↓   Question 3</a>
         <ul>
             <li><a href="#">Réponse 1</a></li>
             <li><a href="#">Réponse 2</a></li>
