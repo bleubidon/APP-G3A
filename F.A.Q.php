77715bb65efa52e06c83c06f5e7b212d3d2ee9a9
Yassine Azeba <yassineazebaaz@gmail.com>
2020-01-13 15:19:42 +0100

    faq 1

---
+++ F.A.Q.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Stylesheet_FAQ.css" />
+    <title>FAQ-Captest</title>
+</head>
+<body>
+
+<p id="title"><h>Questions & Réponses</h></p>
+
+<br>
+
+<ul id="menu-deroulant">
+    <li id="Q"><a href="#">Question 1</a>
+        <ul>
+            <li><a href="#">Réponse 1</a></li>
+            <li><a href="#">Réponse 2</a></li>
+            <li><a href="#">Réponse 3</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#">Question 2</a>
+        <ul>
+            <li><a href="#">Réponse 1</a></li>
+            <li><a href="#">Réponse 2</a></li>
+            <li><a href="#">Réponse 3</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#">Question 3</a>
+        <ul>
+            <li><a href="#">Réponse 1</a></li>
+            <li><a href="#">Réponse 2</a></li>
+            <li><a href="#">Réponse 3</a></li>
+        </ul>
+    </li>
+    <br>
+</ul>
+</body>
