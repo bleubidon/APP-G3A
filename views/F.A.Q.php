51ab57e4ac219dd1f489d6998c68462ba62ce5cf
Yassine Azeba <yassineazebaaz@gmail.com>
2020-01-13 17:05:59 +0100

    faq 2 bis

--- views/F.A.Q.php
+++ views/F.A.Q.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../Stylesheet_FAQ.css" />
+    <link rel="stylesheet" href="../ressources/stylesheets/Stylesheet_FAQ.css" />
     <link rel="stylesheet" href="../ressources/stylesheets/Stylesheet.css"/>
     <title>FAQ-Captest</title>
 </head>
