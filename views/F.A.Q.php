f52584ecbc0e33dac5f4a52e1f33b135e8ea37a1
Yassine Azeba <yassineazebaaz@gmail.com>
2020-01-13 17:05:21 +0100

    faq 2

---
+++ views/F.A.Q.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../Stylesheet_FAQ.css" />
+    <link rel="stylesheet" href="../ressources/stylesheets/Stylesheet.css"/>
+    <title>FAQ-Captest</title>
+</head>
+<?php include "views/Mise_en_page.php" ?>
+<body>
+<br><br><br>
+<p id="title"><h>Questions & Réponses</h></p>
+
+<br>
+
+<ul id="menu-deroulant">
+    <li id="Q"><a href="#" id="Q1">↓   Question 1</a>
+        <ul>
+            <li><a href="#">Réponse</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#" id="Q2">↓   Question 2</a>
+        <ul>
+            <li><a href="#">Réponse</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#" id="Q3">↓   Question 3</a>
+        <ul>
+            <li><a href="#">Réponse</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#" id="Q4">↓   Question 4</a>
+        <ul>
+            <li><a href="#">Réponse</a></li>
+        </ul>
+    </li>
+    <br>
+    <li id="Q"><a href="#" id="Q5">↓   Question 5</a>
+        <ul>
+            <li><a href="#">Réponse</a></li>
+        </ul>
+    </li>
+</ul>
+</body>
