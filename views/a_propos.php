8126d78efa7fc177c072438a4d57b70c014d3626
bleubidon <armand.barral@orange.fr>
2020-01-15 11:11:19 +0100

    Fix style a_propos

--- views/a_propos.php
+++ views/a_propos.php
@@ -1,50 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="Stylesheet" href="/ressources/stylesheets/Stylesheet_a_propos.css">
-    <title>A propos</title>
-</head>
+<?php
+session_start();
+include "header.php";
+include "Mise_en_page.php";
+?>
+
 <body>
-    <section1 id="section1">
-        <div id="imageCaptimove">
-            <img id="logoCaptimove" src="/ressources/images/captimove_logo.png"> <br>
-            <p class="captest"> CAPTEST </p>
-        </div>
+<section2 id="section2">
+    <p id="infoProduit"> INFORMATIONS SUR LE PRODUIT </p>
+    <br><br><br><br>
+    <br><br><br><br>
 
-        <div id="foot">
-            <section3>
-                <br>
-                <p><a href="/controllers/contact_administrateur.php?retour=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">Contacter le
-                    support</a></p>
-                <p> <a href="/controllers/deconnexion.php"> Se déconnecter </a></p>
-                <p><a href="/views/a_propos.html">À propos</a></p>
-                <p><a id="CGU" href="/views/cgu.html">CGU et mentions légales</a></p>
-            </section3>
-        </div>
-    </section1>
+    <p id="coordonnees"> COORDONNEES </p>
+    <br><br><br><br>
+    <br><br><br><br>
 
-    <section2 id="section2">
-        <p id="infoProduit"> INFORMATIONS SUR LE PRODUIT </p>
-        <br><br><br><br>
-        <br><br><br><br>
+    <p id="infoProduitbis"> PLUS D'INFORMATIONS </p>
+    <br><br><br><br>
+    <br><br><br><br>
+    <br><br><br><br>
 
-        <p id="coordonnees">  COORDONNEES </p>
-        <br><br><br><br>
-        <br><br><br><br>
+    <div id="conteneur">
+        <button id="bouton_retour" onclick="window.location.href='page_principale_utilisateur.php' ">Retour</button>
+    </div>
 
-        <p id="infoProduitbis">  PLUS D'INFORMATIONS </p>
-        <br><br><br><br>
-        <br><br><br><br>
-        <br><br><br><br>
-
-        <div id="conteneur">
-            <button id="bouton_retour" onclick="window.location.href='page_principale_utilisateur.php' ">Retour</button>
-        </div>
-
-        <p class="footer"> CAPTEST </p>
-    </section2>
-
+    <p class="footer"> CAPTEST </p>
+</section2>
 
 </body>
 </html>
--- views/admin/gestion_capteurs.php
+++ views/admin/gestion_capteurs.php
@@ -1,4 +1,18 @@
 <?php
 include "../../views/header.php";
 include "../../views/Mise_en_page.php";
+include "../../views/tableau_colore.php";
 ?>
+
+<div id="section_centrale">
+    <table id="table_utilisateurs">
+        <tr>
+            <th class="color1">Nom du capteur</th>
+            <th class="color2">Statut</th>
+        </tr>
+        <tr>
+            <td class="color1">Nom du capteur</td>
+            <td class="color2">Statut</td>
+        </tr>
+    </table>
+</div>
