6656b7f3071b709a2ab63e59760f2174b0311b96
Eric Affoyon <56036507+ericaff@users.noreply.github.com>
2020-01-18 14:40:09 +0100

    légères modifs sur les pages de creation de profil. Reste des ajustements à faire

--- views/creaProfile/creaProfile2.php
+++ views/creaProfile/creaProfile2.php
@@ -1,5 +1,6 @@
 <body>
 <section id="sec1">
+    <br><br><br><br><br><br>
     <?php
     if (isset($modification_profil)) echo "<h1>Modification de vos données de santé</h1>";
     else echo "<h1>Vos données de santé</h1>"; ?>
@@ -9,14 +10,14 @@
             <p>
                 Genre <abbr title="Champ obligatoire">*</abbr><br/>
                 <input type="radio" name="genre" value="Homme"
-                       id="Homme"
+                       id="Homme" size="35"
                     <?php if (isset($modification_profil) && $genre == "Homme") echo "checked";
                     else if (isset($genre) && $genre == "Homme") echo "checked";
                     ?>/>
                 <label for="Homme">Homme</label>
 
                 <input type="radio" name="genre" value="Femme"
-                       id="Femme" ;
+                       id="Femme" ; size="35"
                     <?php if (isset($modification_profil) && $genre == "Femme") echo "checked";
                     else if (isset($genre) && $genre == "Femme") echo "checked";
                     ?>/>
@@ -28,7 +29,7 @@
             <p><label for="Poids">Poids <abbr title="Champ obligatoire; pas d'accents">*</abbr></label>
                 <br>
                 <input type="number" name="poids" id="poids" placeholder=" Votre Poids (kg)"
-                       size="20"
+                       size="35"
                     <?php if (isset($modification_profil)) echo "value=\"$poids\"";
                     else echo "value=\"$poids\"";
                     ?>
@@ -40,7 +41,7 @@
 
             <p><label for="mail">Taille (cm) <abbr title="Champ obligatoire; pas d'accents">*</abbr></label><br>
                 <input type="number" name="taille" id="taille" placeholder=" Votre taille"
-                       size="20"
+                       size="35"
                     <?php if (isset($modification_profil)) echo "value=\"$taille\"";
                     else echo "value=\"$taille\"";
                     ?>
@@ -54,7 +55,7 @@
             <p><label for="mail">Groupe sanguin <abbr title="Champ obligatoire; pas d'accents">*</abbr></label>
                 <br>
                 <input type="text" name="gsang" id="gsang" placeholder=" Votre groupe sanguin"
-                       size="20"
+                       size="35"
                     <?php if (isset($modification_profil)) echo "value=\"$groupe_sanguin\"";
                     else echo "value=\"$gsang\"";
                     ?>
@@ -65,7 +66,7 @@
             </p>
 
             <p><label for="mail">Sommeil moyen <abbr title="Champ obligatoire; pas d'accents">*</abbr></label><br>
-                <input type="text" name="sommeil" id="duréeSommeil" size="20"
+                <input type="text" name="sommeil" id="duréeSommeil" size="35"
                        placeholder="000-000-000"
                     <?php if (isset($modification_profil)) echo "value=\"$sommeil_moyen\"";
                     else echo "value=\"$sommeil\"";
@@ -77,15 +78,16 @@
             </p>
         </div>
         <div id="pathologie">
-            <p><label for="mail">Antécedent / Pathologie</label>
-                <br/>
+            <p><label for="pathologie">Antécedent / Pathologie</label>
+                <br>
                 <textarea name="pathologie" id="pathologie" placeholder="Vos antécedents et/ou pathologies" cols="55"
                           rows="5"><?php if (isset($modification_profil)) echo $pathologie ?></textarea>
         </div>
         <p>
         <div id="boutons">
-            <a class="bouton" href="?etape=1">Retour</a>
-            <input type="submit" value="Continuer"/></div>
+            <a id="bouton_retour" href="?etape=1">Retour</a>
+            &nbsp;<input id="con" type="submit" value="Continuer"/>
+        </div>
         </p>
     </form>
 </section>
