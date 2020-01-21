8507b8befb925c88dcaa58b3aa1c55eeeea6a0f4
vincent <vincent.desmazieres@isep.fr>
2020-01-21 23:15:56 +0100

    modification sql

--- views/gestionnaire/generer_table_emplois_quels_tests.php
+++ views/gestionnaire/generer_table_emplois_quels_tests.php
@@ -2,3 +2,4 @@
 include "../../models/gestionnaire/recup_tableau_emplois_quels_tests.php";
 include "../../models/gestionnaire/recup_tableau_tests_psycho.php";
 echo json_encode(array($liste_emplois_quels_tests, $liste_tests_psycho));
+
